a020c9eeacd7e74330d578ea55b882c0a376dd8b
ALUMNO <ALUMNO@M12-CIV>
2026-04-06 11:37:54 -0600

    Agregar archivos de proyecto.

---
+++ OARR20260406.AppSeguridadWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OARR20260406.AppSeguridadWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
