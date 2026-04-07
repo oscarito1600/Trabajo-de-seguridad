a6206b9f0da703fdc347a7affa6b3b1e6c3ef300
Oscar <barleyunmain@gmail.com>
2026-04-07 08:22:07 -0600

    Se modifico el archivo Program.cs using

--- OARR20260406.AppSeguridadWeb/Program.cs
+++ OARR20260406.AppSeguridadWeb/Program.cs
@@ -1,7 +1,15 @@
+
+using Microsoft.EntityFrameworkCore;
+using OARR20260406.AppSeguridadWeb.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configurar la conexión a SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
