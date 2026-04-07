d26193e5dba4f70d2dac9d072c453a4f62cf0bb6
Oscar <barleyunmain@gmail.com>
2026-04-07 08:13:27 -0600

    Se creo el archivo AppDbContext.cs y se arreglo el codigo del archivo usuario.

--- OARR20260406.AppSeguridadWeb/Models/Usuario.cs
+++ OARR20260406.AppSeguridadWeb/Models/Usuario.cs
@@ -1,6 +1,33 @@
-﻿namespace OARR20260406.AppSeguridadWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OARR20260406.AppSeguridadWeb.Models
 {
+    [Table("Usuarios")]
     public class Usuario
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre de usuario es obligatorio")]
+        [StringLength(50)]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string PasswordHash { get; set; }
+        [NotMapped]
+        [Required(ErrorMessage = "Debe confirmar la contraseña")]
+        [Compare("PasswordHash", ErrorMessage = "Las contraseñas no coinciden")]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar Contraseña")]
+        public string ConfirmarPassword { get; set; }
+        [Required]
+        [StringLength(20)]
+        [AllowedValues("Administrador", "Editor", "Usuario", ErrorMessage = "Seleccione un rol válido")]
+        public string Rol { get; set; }
+        public bool EstaActivo { get; set; } = true;
     }
 }
